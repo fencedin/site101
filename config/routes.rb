2ae2fc2bc1ddfdb4cb424bcaa9b7b2732358efdd
Vincent <vincent.rotter@gmail.com>
2014-04-20 21:40:21 -0700

    remove admin for heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Site101::Application.routes.draw do
   root to: 'main#index'
-  devise_for :admins
+  devise_for :admins, :skip => ['passwords', 'registrations']
   resources :main, only: [:index, :show]
   resources :wikis, except: [:new, :edit] do
     resources :comments, only: [:create]
   end
-  resources :tags, only: [:index, :create, :update, :destroy]
+  resources :tags, only: [:index, :create, :destroy]
 end
