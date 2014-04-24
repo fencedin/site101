4599790ff95f3b33d2142ca861cce608250d728c
Vincent <vincent.rotter@gmail.com>
2014-04-23 21:04:06 -0700

    update path to google partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require analytics
 //= require jquery
-//= require jquery_ujs
 //= require jquery.turbolinks
-//= require_tree .
+//= require jquery_ujs
 //= require bootstrap
+//= require analytics
+//= require turbolinks
+//= require_tree .
